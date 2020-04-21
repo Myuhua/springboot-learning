563bddf67892591a4929f007e170f96d8c207425
yuhua <hua.yu@sdgharm.com>
2020-04-21 14:51:52 +0800

    项目重构，加入父项目工程

---
+++ springboot-shiro-session/src/main/java/com/yuhua/shiro/ShiroApplication.java
@@ -0,0 +1,16 @@
+package com.yuhua.shiro;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(basePackages = {"com.yuhua.shiro.**.mapper"}) //扫描DAO
+public class ShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroApplication.class, args);
+    }
+
+}
