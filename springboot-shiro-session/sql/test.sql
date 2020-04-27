7503dd6698897a57db5597c9a8c0cfe5059daa24
yuhua <hua.yu@sdgharm.com>
2020-04-27 16:04:20 +0800

    sql更新

--- springboot-shiro-session/sql/test.sql
+++ springboot-shiro-session/sql/test.sql
@@ -1,17 +1,17 @@
 /*
- Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
- Source Server         : 我的腾讯云
+ Source Server         : 241
  Source Server Type    : MySQL
- Source Server Version : 50725
- Source Host           : 140.143.244.170:3306
- Source Schema         : my_shiro
+ Source Server Version : 50727
+ Source Host           : 172.16.11.241:3306
+ Source Schema         : test
 
  Target Server Type    : MySQL
- Target Server Version : 50725
+ Target Server Version : 50727
  File Encoding         : 65001
 
- Date: 20/06/2019 11:05:03
+ Date: 27/04/2020 16:03:55
 */
 
 SET NAMES utf8mb4;
@@ -23,8 +23,8 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `sys_menu`;
 CREATE TABLE `sys_menu`  (
   `menu_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '权限ID',
-  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '权限名称',
-  `perms` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '权限标识',
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '权限名称',
+  `perms` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '权限标识',
   PRIMARY KEY (`menu_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '权限表' ROW_FORMAT = Dynamic;
 
@@ -58,8 +58,8 @@ INSERT INTO `sys_role` VALUES (2, 'USER');
 DROP TABLE IF EXISTS `sys_role_menu`;
 CREATE TABLE `sys_role_menu`  (
   `id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `role_id` bigint(11) NULL DEFAULT NULL COMMENT '角色ID',
-  `menu_id` bigint(11) NULL DEFAULT NULL COMMENT '权限ID',
+  `role_id` bigint(11) DEFAULT NULL COMMENT '角色ID',
+  `menu_id` bigint(11) DEFAULT NULL COMMENT '权限ID',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '角色与权限关系表' ROW_FORMAT = Dynamic;
 
@@ -78,17 +78,19 @@ DROP TABLE IF EXISTS `sys_user`;
 CREATE TABLE `sys_user`  (
   `user_id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
   `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
-  `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
-  `salt` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '盐值',
-  `state` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '状态:NORMAL正常  PROHIBIT禁用',
+  `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '密码',
+  `salt` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '盐值',
+  `state` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '状态:NORMAL正常  PROHIBIT禁用',
   PRIMARY KEY (`user_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统用户表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1251058107010838530 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统用户表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES (1, 'admin', 'a1bb09ad5dea12e0f94762cb116c447e80c784d8aa2c6625263f7f3436cdd583', 'RvP3UID2n30Q2sycZYvH', 'NORMAL');
-INSERT INTO `sys_user` VALUES (2, 'user', '376eb5d2698c804ee83594fe8b0217f03ad138a046f7fa42b44c232c2e5e2b38', 'OVlrD37bDUKNcFRB10qG', 'NORMAL');
+INSERT INTO `sys_user` VALUES (1, 'admin', 'a1bb09ad5dea12e0f94762cb116c447e80c784d8aa2c6625263f7f3436cdd583', 'RvP3UID2n30Q2sycZYvH', '0');
+INSERT INTO `sys_user` VALUES (2, 'user', '376eb5d2698c804ee83594fe8b0217f03ad138a046f7fa42b44c232c2e5e2b38', 'OVlrD37bDUKNcFRB10qG', '0');
+INSERT INTO `sys_user` VALUES (1251057997518532609, 'user1', 'af046cf2ab62a60e0bd8d9ba878a642b75605fd721be53c60c7c8ee8ba1a1d70', 'YzNMesJkcqxyXbkbnaUH', '0');
+INSERT INTO `sys_user` VALUES (1251058107010838529, 'user1', 'e8bf854dea2be312b85c67c7213929c397c69b1e41038b266f2857aecedbfd4b', 'aqUoiEDgZWXsWqRU5nTF', '0');
 
 -- ----------------------------
 -- Table structure for sys_user_role
@@ -96,15 +98,39 @@ INSERT INTO `sys_user` VALUES (2, 'user', '376eb5d2698c804ee83594fe8b0217f03ad13
 DROP TABLE IF EXISTS `sys_user_role`;
 CREATE TABLE `sys_user_role`  (
   `id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `user_id` bigint(11) NULL DEFAULT NULL COMMENT '用户ID',
-  `role_id` bigint(11) NULL DEFAULT NULL COMMENT '角色ID',
+  `user_id` bigint(11) DEFAULT NULL COMMENT '用户ID',
+  `role_id` bigint(11) DEFAULT NULL COMMENT '角色ID',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户与角色关系表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1251058107048587267 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户与角色关系表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_user_role
 -- ----------------------------
 INSERT INTO `sys_user_role` VALUES (1, 1, 1);
 INSERT INTO `sys_user_role` VALUES (2, 2, 2);
+INSERT INTO `sys_user_role` VALUES (1251057997774385154, 1251057997518532609, NULL);
+INSERT INTO `sys_user_role` VALUES (1251058107048587266, 1251058107010838529, NULL);
+
+-- ----------------------------
+-- Table structure for t_student
+-- ----------------------------
+DROP TABLE IF EXISTS `t_student`;
+CREATE TABLE `t_student`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `number` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_student
+-- ----------------------------
+INSERT INTO `t_student` VALUES (1, 'yuhua', '121314');
+INSERT INTO `t_student` VALUES (2, '小明', '12345');
+INSERT INTO `t_student` VALUES (3, '小华', '123456');
+INSERT INTO `t_student` VALUES (4, '王帅', '5345');
+INSERT INTO `t_student` VALUES (5, '张强', '534534');
+INSERT INTO `t_student` VALUES (6, '李四', '455');
+INSERT INTO `t_student` VALUES (7, '马六', '7657');
 
 SET FOREIGN_KEY_CHECKS = 1;
